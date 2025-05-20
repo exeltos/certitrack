ea480591381aeff184a2d6fabf4fc035a13c94a3
exeltos <info@exeltos.com>
2025-05-20 13:59:39 +0300

    Delete certitrack-dist directory

--- certitrack-dist/js/config.js
+++
@@ -1,4 +0,0 @@
-// js/config.js
-
-export const SUPABASE_URL = 'https://klutmusrabsizqjnzwpu.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsdXRtdXNyYWJzaXpxam56d3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1NjUzMzAsImV4cCI6MjA2MjE0MTMzMH0.wU80uKyWW9iuNm0_6ghtvIJ_6SFMCv3RhSYodPMZOeg'; // βεβαιώσου ότι δεν έχει κενά ή line-breaks μέσα στο string
--- certitrack-dist/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-// Αρχικοποίηση Supabase client με τις μεταβλητές ρυθμίσεων
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
